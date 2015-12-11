af848389878027420440ed6e6d00735631498023
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-12-11 15:48:59 -0500

    Create wrench struct which holds the properties of the wrenches (upper head, lower head and 2D length), Find the smallest wrench, mark it, spit out from terminal

--- src/construct_wrenches.cpp
+++ src/construct_wrenches.cpp
@@ -20,17 +20,24 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-void circleCallback(const std_msgs::Float64MultiArray::ConstPtr& mat_msg);
+struct wrench {
+  cv::Point2f upperHead2D;
+  cv::Point2f lowerHead2D;
+  float length2D;
+}
+
+void wrenchCallback(const std_msgs::Float64MultiArray::ConstPtr& mat_msg);
 void BGRCallback(const sensor_msgs::ImageConstPtr& msg);
 
 cv::Mat circles, frameBGR;
+std::vector<wrench> wrenches;
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "construct_wrenches");
   ros::NodeHandle nh;
   image_transport::ImageTransport BGRTransport(nh);
 
-  ros::Subscriber sub = nh.subscribe("/center_indices_by_votes", 1000, circleCallback);
+  ros::Subscriber sub = nh.subscribe("/center_indices_by_votes", 1000, wrenchCallback);
   image_transport::Subscriber BGRSubscriber = BGRTransport.subscribe("/camera/rgb/image_rect_color", 1, BGRCallback);
   ros::spin();
 
@@ -52,23 +59,44 @@ void BGRCallback(const sensor_msgs::ImageConstPtr& msg) {
   cv::waitKey(3);
 }
 
-void circleCallback(const std_msgs::Float64MultiArray::ConstPtr& mat_msg) {
+void wrenchCallback(const std_msgs::Float64MultiArray::ConstPtr& mat_msg) {
   circles = cv::Mat::zeros(mat_msg->layout.dim[0].size, mat_msg->layout.dim[1].size, CV_32FC1);
   cv::Mat canvas = frameBGR.clone();
+  std::vector row;
+  wrenches.clear();
   // msg->layout.dim[0] -> number of rows
   // msg->layout.dim[1] -> number of columns
-  for (int i=0;i<mat_msg->layout.dim[0].size;++i){
-      for (int j=0;j<mat_msg->layout.dim[1].size;++j){
-          circles.at<float>(i,j) = mat_msg->data[mat_msg->layout.dim[1].stride*i + j]; //mat_msg->layout.data_offset +
-      }
+	for (int i=0;i<mat_msg->layout.dim[0].size;++i){
+  		for (int j=0;j<mat_msg->layout.dim[1].size;++j){
+    		row.push_back(mat_msg->data[mat_msg->layout.dim[1].stride*i + j]);
+        	circles.at<float>(i,j) = mat_msg->data[mat_msg->layout.dim[1].stride*i + j]; //mat_msg->layout.data_offset +
+		}
+		  wrench Wrench;
+		  Wrench.upperHead2D = cv::Point2f(row[0], row[1]);
+		  Wrench.lowerHead2D = cv::Point2f(row[0], row[1]);
+		  Wrench.length2D = cv::norm(Wrench.upperHead2D-Wrench.lowerHead2D);
 
-      cv::Point center = cv::Point((int)circles.at<float>(i,1), (int)circles.at<float>(i,0));
+  	wrenches.push_back(Wrench);
+	cv::Point center = cv::Point((int)circles.at<float>(i,1), (int)circles.at<float>(i,0));
 
-      cv::circle(canvas, center, circles.at<float>(i,2), cv::Scalar(0,0,255), 2);
-  }
+	cv::circle(canvas, center, circles.at<float>(i,2), cv::Scalar(0,0,255), 2);
+ 	}
 
-  cv::imshow("Canvas", canvas);
-  cv::waitKey(3);
+  float minLength = -1;
+  size_t minID;
+
+for(int i=0; i<wrenches.size(); i++) {
+	if(wrenches[i].lenghth2d<minLength){
+		minLength = wrenches[i].lenghth2d;
+		minID =i;
+	}
+}
+
+
+	cv::circle(canvas, center, circles.at<float>(i,2), cv::Scalar(0,255,0), 2);
+	std::cout<<"Wrench Found at:"<<(wrenches[minID].upperHead2D+wrenches[minID].lowerHead2D)/2<<std::endl;
+	cv::imshow("Canvas", canvas);
+	cv::waitKey(3);
 
   std::cout << "matrix recieved" << std::endl;
 }
